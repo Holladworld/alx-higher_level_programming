017f5572156ea16bcbaba66bdcc234425e66de0e
Holladworld <oladimejiolayinka@gmail.com>
2023-08-19 09:54:12 -0700

    12-roman to integer

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -2,9 +2,10 @@
 def roman_to_int(roman_string):
     roman_dict =  {'M': 1000, 'D': 500, 'C': 100, 'L': 50, 'X': 10, 'V': 5, 'I': 1}
     sumTotal = 0
-    for Roman_num in reversed(roman_string):
-        roman = roman_dict[Roman_num]
+    for roman_num in reversed(roman_string):
+        roman = roman_dict[roman_num]
         sumTotal += roman if sumTotal < roman * 5 else -roman
+
     if not roman_string or type(roman_string) != str:
-        return 0
+            return 0
     return sumTotal
