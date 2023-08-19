b7a69f1507701353a268d4ee66c7ceea9be212d2
Holladworld <oladimejiolayinka@gmail.com>
2023-08-19 07:51:31 -0700

    task upate

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
+
 def search_replace(my_list, search, replace):
     new_list = my_list[:]
     for i in range(len(new_list)):
-        if new_list [i] == search:
+        if new_list[i] == search:
             new_list[i] == replace
-    return  my_list
+    return  new_list
