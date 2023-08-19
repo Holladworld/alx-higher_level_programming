168dd1419fe74df9b9240148c4a4191269f8ce83
Holladworld <oladimejiolayinka@gmail.com>
2023-08-19 09:35:31 -0700

    advanced tasks

---
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def weight_average(my_list=[]):
+    num = 0
+    base_num = 0
+    for tup in my_list:
+        num += tup[0] * tup[1]
+        base_num += (tup[1])
+    return float(num/ base_num)
+    if not my_list:
+        return 0
+    
---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+    roman_dict =  {'M': 1000, 'D': 500, 'C': 100, 'L': 50, 'X': 10, 'V': 5, 'I': 1}
+    sumTotal = 0
+    for Roman_num in reversed(roman_string):
+        roman = roman_dict[Roman_num]
+        sumTotal += roman if sumTotal < roman * 5 else -roman
+    if not roman_string or type(roman_string) != str:
+        return 0
+    return sumTotal
