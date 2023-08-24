bd28a846fa6b69292541dde5fa85455ff291db60
Holladworld <oladimejiolayinka@gmail.com>
2023-08-24 09:53:39 -0700

    check lower characters

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,10 +1,7 @@
 #!/usr/bin/python3
-for numbers in range(0, 100):
-    digits = numbers % 10
-    num = numbers // 10
-    if digits > num != num:
-        print(":02d}".format(numbers), end="")
-        if numbers != 89:
-            print(", ", end="")
+for numbers in range(0, 10):
+    for digit in range(x + 1, 10):
+        if numbers == 8 and digit == 9:
+            print('89')
         else:
-            print("\n", end="")
+            print('{}{}, '.format(numbers, digit), end='')
