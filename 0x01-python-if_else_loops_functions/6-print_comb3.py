0590e09bc902f759c60fe513deb0aa82ad0d9431
Holladworld <oladimejiolayinka@gmail.com>
2023-08-24 09:32:13 -0700

    print_comb3

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 for numbers in range(9):
-    for digit in range(number + 1, 10)
+    for digit in range(number + 1, 10):
         if numbers * 10 + digits < 89:
             print("{:d}{:d}".format(numbers, digits), end=", ")
 print("{:d}".format(89))
