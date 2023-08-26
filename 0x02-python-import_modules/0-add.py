7941994c55709dfcb986dcabb90667607adb473e
Holladworld <oladimejiolayinka@gmail.com>
2023-08-26 07:10:37 -0700

    task 1

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,11 +1,7 @@
 #!/usr/bin/python3
-
-if __name__ == '__main__':
-    # Write a program that imports the function def add(a, b):
-    # from the file add_0.py and prints the
-    # result of the addition 1 + 2 = 3
-    """Print some of 1 and 2"""
+if __name__ == "__main__":
     from add_0 import add
+
     a = 1
     b = 2
-    print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
+    print("{} + {} = {}".format(a, b, add(a,b)))
