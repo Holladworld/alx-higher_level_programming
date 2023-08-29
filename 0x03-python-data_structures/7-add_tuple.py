03a81f2580b59a8d4808903bbe1c139b00a38b6f
Holladworld <oladimejiolayinka@gmail.com>
2023-08-29 07:00:19 -0700

    corrected task 8

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -4,7 +4,7 @@
 def add_tuple(tuple_a=(), tuple_b=()):
     first_elem, second_elem = len(tuple_a), len(tuple_b)
     added_tuple = ((tuple_a[0] if first_elem >= 1 else 0) +
-                 (tuple_b[0] if second_elem >= 1 else 0),
-                 (tuple_a[1] if first_elem >= 2 else 0) +
-                 (tuple_b[1] if second_elem >= 2 else 0))
+                   (tuple_b[0] if second_elem >= 1 else 0),
+                   (tuple_a[1] if first_elem >= 2 else 0) +
+                   (tuple_b[1] if second_elem >= 2 else 0))
     return added_tuple
--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-# eturns a tuple with the length of a string and its first character.
+# Returns a tuple with the length of a string and its first character.
 def multiple_returns(sentence):
-    if sentence != '':
-        return (len(sentence), sentence[0] if len(sentence) > 0 else None)
+    return len(sentence), sentence[0] if len(sentence) > 0 else None
