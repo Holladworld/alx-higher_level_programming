6a465e614f9105036351de14f32784c1bf414412
Holladworld <oladimejiolayinka@gmail.com>
2023-08-29 08:14:16 -0700

    funtions that define a square

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
-"Defines a class Square"
+'''Define a class Square.'''
 
 
 class Square:
-    """Class Square that defines a square:
-    -Private instance attribute: size
-    -Instantiation with size (no type/value verification)"""
+    '''Class that represents a square:
+    -initializez new Square.
+
+    size: size of square (not type/value verification)'''
     def __init__(self, size):
         self.__size = size
--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
-"Defines a class Square"
+'''Defines a class Square'''
 
 
 class Square:
-    """Class Square that defines a square
-    Private instance attribute: size
-    Instantiation with optional size"""
+    '''Class Square represent  a square
+    Private instance attribute: size'''
     def __init__(self, size=0):
+        '''initialize new square'''
         if type(size) is not int:
             raise TypeError("size must be an integer")
         elif size < 0:
