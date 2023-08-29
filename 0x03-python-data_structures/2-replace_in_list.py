56d698d2600e1b03d1f29e9c8930bd74e9256e16
Holladworld <oladimejiolayinka@gmail.com>
2023-08-29 05:44:10 -0700

    added 0-2 task

---
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+# replaces an element of a list at a specific position (like in C)
+def replace_in_list(my_list, idx, element):
+    if idx >= 0 and idx < len(my_list):
+        my_list[idx] = element
+        return (my_list)
