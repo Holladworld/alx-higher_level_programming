93f465760c500d732d833032457322a49071eb49
Holladworld <oladimejiolayinka@gmail.com>
2023-08-29 08:21:30 -0700

    3-square

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -1,10 +1,14 @@
 #!/usr/bin/python3
-"Defines a class Square"
+# class Square that defines a square by: (based on 2-square.py)
+'''Defines a class Square.'''
 
 
 class Square:
-    """Class Square that defines a square"""
+    """Class Square that reprents a square"""
+
     def __init__(self, size=0):
+        '''initialize new square'''
+
         if type(size) is not int:
             raise TypeError("size must be an integer")
         elif size < 0:
