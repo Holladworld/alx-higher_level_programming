dbdf59bf9e2c2e63499d2a1b875e6cb7b9d4292e
Holladworld <oladimejiolayinka@gmail.com>
2023-08-29 07:27:56 -0700

    corrected tasks

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -3,6 +3,6 @@
 def max_integer(my_list=[]):
     int_list = my_list
     if int_list:
-        int_list.sort(reverse = True)
+        int_list.sort(reverse=True)
         return (int_list[0])
     return (None)
