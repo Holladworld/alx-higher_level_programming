bd149df16bcc8f0c2814161d4bcbb575735ed4bb
Holladworld <oladimejiolayinka@gmail.com>
2023-09-17 19:19:08 -0700

    magic string

---
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+'''Module that defines locked class'''
+
+
+class LockedClass:
+    '''
+     A class `LockedClass` with no class or object attribute, that prevents the user from dynamically creating new instance attributes, except if the new instance attribute is called `first_name
+     '''
+    __slots__ = ['first_name']
