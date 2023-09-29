7d7c432f59a10160e575b17c2b7e552a19e5f92c
Holladworld <oladimejiolayinka@gmail.com>
2023-09-29 07:16:20 -0700

    corrected errors

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-''' 
+'''
 Module for function inherits_from
 '''
 
@@ -10,7 +10,9 @@ def inherits_from(obj, a_class):
     Args:
         obj: Object initiated
         a_class: class to be inherited
-        Return: True if the object is an instance of a class that inherited (directly or indirectly) from the specified class;
+        Return: True if the object is an instance of a class
+        that inherited (directly or indirectly) from the specified
+        class;
         otherwise False
         '''
     return issubclass(type(obj), a_class) and type(obj) is not a_class
