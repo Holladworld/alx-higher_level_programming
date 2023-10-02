f4555394da814eae0d589a5fe0608696ff28350e
Holladworld <oladimejiolayinka@gmail.com>
2023-10-02 00:44:53 -0700

    space adjustment for write file

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -5,7 +5,7 @@ Module for write_file  method
 
 
 def write_file(filename="", text=""):
-   '''Method that write a string to a text file'''
-   with open(filename, "w", encoding="utf-8") as my_file_1:
+    '''Method that write a string to a text file'''
+    with open(filename, "w", encoding="utf-8") as my_file_1:
         files = my_file_1.write(text)
         return (files)
