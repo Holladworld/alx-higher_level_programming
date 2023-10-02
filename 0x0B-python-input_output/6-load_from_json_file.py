825e51dbfd310532c253ffb78d10fe1c37942a47
Holladworld <oladimejiolayinka@gmail.com>
2023-10-02 03:01:11 -0700

    load to json file added

--- 0x0B-python-input_output/6-load_from_json_file.py
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
-"""Module for load_from_json_file method"""
+'''Module for load_from_json_file method'''
+
 import json
 
 
 def load_from_json_file(filename):
-    """Method that creates an Object from a “JSON file”"""
-    with open(filename, "r", encoding="utf-8") as file_load:
-        return json.load(file_load)
+    '''This method that creates an Object from a “JSON file”'''
+    with open(filename, "r", encoding="utf-8") as Json_file:
+        obj = json.load(Json_file)
+        return obj
