72fbb78695529c50c5c0b62320096a78f401df26
Holladworld <oladimejiolayinka@gmail.com>
2023-10-02 01:04:30 -0700

    to json string file Added

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+'''
+Module for to_json_string Method
+'''
+
+
+import json
+
+
+def to_json_string(my_obj):
+        '''
+        Method that returns the JSON
+        representation of an object (string)
+        '''
+        return json.dumps(my_obj)
