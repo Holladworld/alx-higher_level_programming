ced5f88d3d42eae4e33e5b329f2615260da997cf
Holladworld <oladimejiolayinka@gmail.com>
2023-10-02 00:14:44 -0700

    Added directories, README and 0-read_file

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+'''
+Module for read_file method
+'''
+
+
+def read_file(filename=""):
+    '''Reads a text file (UTF8) and prints it to stdout'''
+    with open(filename, "r", encoding="utf-8") as file:
+        print(file.read(), end="")
