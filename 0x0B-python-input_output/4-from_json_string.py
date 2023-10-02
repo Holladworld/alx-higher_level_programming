db9be32580efab7ace20242a39f7dc54f8fb7253
Holladworld <oladimejiolayinka@gmail.com>
2023-10-02 01:14:32 -0700

    Added 4-from json file

---
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+''' Module container of 'from_json_string'''
+import json
+
+
+def from_json_string(my_str):
+    '''Returns an object (Python data structure)
+    represented by a JSON string'''
+    return json.loads(my_str)
