c046f790f4b624980d6c0a05f4c26d26d687c41c
Holladworld <oladimejiolayinka@gmail.com>
2023-10-02 01:06:43 -0700

    to json string file Added

--- 0x0B-python-input_output/3-to_json_string.py
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -8,8 +8,8 @@ import json
 
 
 def to_json_string(my_obj):
-        '''
-        Method that returns the JSON
-        representation of an object (string)
-        '''
-        return json.dumps(my_obj)
+    '''
+    Method that returns the JSON
+    representation of an object (string)
+    '''
+    return json.dumps(my_obj)
