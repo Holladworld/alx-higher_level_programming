c4269bccc7b707c0c995d1e784a1f56d4eb762ab
Holladworld <oladimejiolayinka@gmail.com>
2023-10-21 04:56:13 -0700

    script that computes the score average of all records in the table

---
+++ 0x0D-SQL_introduction/14-average.sql
@@ -0,0 +1,4 @@
+-- script that computes the score average of all records in second_table
+-- The result column name should be average
+-- The database name is passed as an argument of the mysql command
+SELECT AVG(score) AS "average" FROM second_table;
