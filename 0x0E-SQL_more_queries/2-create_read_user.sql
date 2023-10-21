8ca24539bba68f14b6d36d51b340872170741798
Holladworld <oladimejiolayinka@gmail.com>
2023-10-21 05:49:14 -0700

    SQL file for create, read user

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -2,8 +2,8 @@
 CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
 CREATE USER IF NOT EXISTS 'user_0d_02'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
 FLUSH PRIVILEGES;
--- graning privileges
+-- granting privileges
 
 GRANT SELECT ON hbtn_0d_2.*
-TO 'user_0d_2'@'localhost'
+TO 'useri_0d_2'@'localhost';
 FLUSH PRIVILEGES;
