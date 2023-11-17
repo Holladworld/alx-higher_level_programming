ba024d6009587e5381b378d3ba3bdda66afe9374
Holladworld <oladimeiolayinka9@gmail.com>
2023-11-17 16:14:43 +0000

    Cities by states

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+'''script that lists all cities from the database hbtn_0e_4_usa'''
+
+import MySQLdb
+import sys
+
+if __name__ == '__main__':
+    # Get the state name
+    sn = sys.argv[4]
+    # Connect to the database
+    db = MySQLdb.connect(host='localhost', port=3306, user=sys.argv[1],
+                         passwd=sys.argv[2], db=sys.argv[3])
+    # creating a cursor object using cursor() method
+    cursor = db.cursor()
+    # Execute the SQL query to get all states that match the user input
+    cursor.execute("SELECT * FROM states WHERE name=%s\
+                   ORDER BY id ASC", (sn,))
+    # Fetch all the rows and print them out
+    rows = cursor.fetchall()
+    for row in rows:
+        print(row)
+
+    # Close the database connection
+    cursor.close()
+    db.close()
