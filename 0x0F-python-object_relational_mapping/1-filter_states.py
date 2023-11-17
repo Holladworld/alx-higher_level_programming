d64573d929a285fa67ad8dac26092b92b96c653c
Holladworld <oladimeiolayinka9@gmail.com>
2023-11-17 16:00:25 +0000

    Filter states by user input

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
-
-
 ''' lists all states with a name starting with N (upper N) from the database hbtn_0e_0_usa'''
+
+
 import MySQLdb
 import sys
+
 
 if __name__ == "__main__":
     conn = MySQLdb.connect(host='localhost', port=3306, user=sys.argv[1],
