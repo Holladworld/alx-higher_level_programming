1d0139927764a3fb2e963258f10c06c4785dcc69
Holladworld <oladimejiolayinka@gmail.com>
2023-11-19 09:35:37 -0800

    unsure

---
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+'''python file that contains the class definition of a State and an instance Base = declarative_base()'''
+
+
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import create_engine
+
+
+Base = declarative_base()
+
+
+class State(Base):
+    '''State class:
+            inherits from Base Tips
+            links to the MySQL table states
+            class attribute id that represents a column of an 
+     auto-generated, unique integer, can’t be null and is a primary key
+            class attribute name that represents a column of a string 
+    with maximum 128 characters and can’t be null'''
+
+    __tablename__ = "states"
+    id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
+    name = Column(String(128), nullable=False)
